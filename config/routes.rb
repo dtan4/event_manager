818b900a6b30c217e35c96561d77ff44fab91e95
dtan4 <dtanshi45@gmail.com>
2014-06-26 03:09:01 +0900

    Show error page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :events do
-    resources :tickets
+  resources :events, except: :index do
+    resources :tickets, only: [:new, :create, :destroy]
   end
 
-  resource :user do
+  resource :user, only: :destroy do
     get "retire"
   end
 
   root to: "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
   get "/logout" => "sessions#destroy", as: :logout
+
+  match "*path" => "application#error404", via: :all
 end
