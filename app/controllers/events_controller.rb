bec3b21442e6ec24f59a4e6f6346f8f349254162
dtan4 <dtanshi45@gmail.com>
2014-06-26 02:51:00 +0900

    Cancel event attendance

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
-  before_action :authenticate
+  before_action :authenticate, except: :show
 
   def show
     @event = Event.find(params[:id])
+    @ticket = current_user && current_user.tickets.find_by(event_id: params[:id])
     @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
